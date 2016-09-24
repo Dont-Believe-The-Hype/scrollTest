13f5c244a3acc6921278d345cd9f0cb5d4e8d911
MeuToy <sim.meurer@gmx.de>
2016-09-24 09:30:01 +0200

    Update controllers.js

--- controllers.js
+++ controllers.js
@@ -3,6 +3,7 @@ angular.module('vpApp', [])
 .controller('vpCtrl', ['$scope', '$http', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
+// nur ein Kommentar
 
 function ($scope, $http) {
         console.log("vpCtrl start!");
